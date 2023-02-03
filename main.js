1b35eceeaca39e4a527bea53f0cea60f0802d10a
Fonkeyy <113917387+Fonkeyy@users.noreply.github.com>
2023-02-03 09:56:39 +0100

    Still not working properly

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let a = '';
 let b = '';
+let c = '';
+let newOperator = '';
 let operator = '';
 let result = '';
 let counter = 0;
@@ -12,17 +14,44 @@ numbersButton.forEach((button) => {
   button.addEventListener('click', addNumber);
 });
 
+/*window.addEventListener('keydown', checkKey);
+
+function checkKey(e) {
+  if (
+    `.key[data-key="${e.keyCode}"]` == '192' ||
+    '111' ||
+    '106' ||
+    '109' ||
+    '107'
+  ) {
+    addOperator(e);
+  } else if (`.number[data-key="${e.keyCode}"]` == '46') {
+    clear();
+  } else if (`.number[data-key="${e.keyCode}"]` == '13') {
+    operate(e);
+  } else {
+    addNumber(e);
+  }
+}*/
+
 function addNumber(e) {
+  // const keyNumber = document.querySelector(`.number[data-key="${e.keyCode}"]`);
   currentNumber.textContent += e.target.textContent;
+  // currentNumber.textContent += keyNumber.value;
   if (counter == 0) {
     a += parseInt(e.target.textContent);
+  } else if (counter == 1) {
+    b += parseInt(e.target.textContent);
+  } else if (counter > 1 && counter % 2 == 0) {
+    c += parseInt(e.target.textContent);
   } else {
     b += parseInt(e.target.textContent);
   }
-  console.log(a);
-  console.log(b);
-  console.log(operator); //Fonctionne dans la fonction mais pas au global
-  console.log(result);
+  console.log('a:', a);
+  console.log('b:', b);
+  console.log('c:', c);
+  console.log('operator:', operator);
+  console.log('counter:', counter);
 }
 
 const operatorsButton = document.querySelectorAll('.operator');
@@ -33,8 +62,24 @@ operatorsButton.forEach((button) => {
 function addOperator(e) {
   previousNumber.textContent +=
     currentNumber.textContent + ' ' + e.target.textContent + ' ';
-  operator += e.target.textContent;
+  newOperator = e.target.textContent;
   counter++;
+  // operate(operator, a, b);
+  // a.textContent = currentNumber.textContent;
+
+  if (counter >= 1 && c == '' /*counter % 2 == 0*/) {
+    // a = parseInt(a) + parseInt(b);
+    operate(operator, a, b);
+    a = result;
+    b = '';
+  } else if (counter > 1 && b == '' /*counter % 2 !== 0*/) {
+    // a = parseInt(a) + parseInt(c);
+    operate(operator, a, parseInt(c));
+    a = result;
+    c = '';
+  }
+  operator = newOperator;
+  newOperator = '';
   clearCurrent();
 }
 
@@ -43,25 +88,27 @@ function clearCurrent() {
 }
 
 const equalButton = document.querySelector('.equal');
-equalButton.addEventListener('click', operate);
+equalButton.addEventListener('click', () => operate(operator, a, b));
 
 function operate(operator, a, b) {
-  previousNumber.textContent += currentNumber.textContent;
+  // previousNumber.textContent += currentNumber.textContent;
+  clearCurrent();
   switch (operator) {
     case '+':
-      result = add(a, b);
+      result = add(parseInt(a), parseInt(b));
       break;
     case '-':
-      result = substract(a, b);
+      result = substract(parseInt(a), parseInt(b));
       break;
     case '*':
-      result = multiply(a, b);
+      result = multiply(parseInt(a), parseInt(b));
       break;
     case '/':
-      result = divide(a, b);
+      result = divide(parseInt(a), parseInt(b));
       break;
   }
   currentNumber.textContent = result;
+  console.log('result:', currentNumber.textContent);
 }
 
 function add(a, b) {
@@ -80,10 +127,10 @@ function divide(a, b) {
   return a / b;
 }
 
-console.log(a);
-console.log(b);
-console.log(operator);
-console.log(result);
+// console.log(a);
+// console.log(b);
+// console.log(operator);
+// console.log(result);
 
 /*function equals() {
   previousNumber.textContent += currentNumber.textContent + ' ' + '=';
@@ -112,6 +159,9 @@ const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', clear);
 
 function clear() {
+  counter = 0;
+  a = '';
+  b = '';
   currentNumber.textContent = '';
   previousNumber.textContent = '';
 }
