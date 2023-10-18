7c7d88d67ac1c68d70e3fd09440b3f51c03b357e
Fonkeyy <cedrick.guevel@gmail.com>
2023-10-18 16:35:27 +0200

    finish and improve logic + keyboard nav

--- main.js
+++ main.js
@@ -1,167 +1,115 @@
 let a = '';
-let b = '';
-let c = '';
-let newOperator = '';
 let operator = '';
 let result = '';
-let counter = 0;
 
 let previousNumber = document.querySelector('#previous-number');
 let currentNumber = document.querySelector('#current-number');
 
-const numbersButton = document.querySelectorAll('.number');
-numbersButton.forEach((button) => {
-  button.addEventListener('click', addNumber);
+const handleClickNumberButton = (e) => {
+    console.log(a);
+    if (typeof e.target === 'object') {
+        a += e.target.textContent;
+    } else if (typeof e === 'string') {
+        a += e;
+    }
+    currentNumber.textContent = a;
+};
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', handleClickNumberButton);
 });
 
-/*window.addEventListener('keydown', checkKey);
+const handleClickOperatorButton = (e) => {
+    if (result) {
+        result = operate(operator, result, parseInt(a));
+    } else {
+        result = parseInt(a);
+    }
+    if (typeof e.target === 'object') {
+        operator = e.target.textContent;
+    } else if (typeof e === 'string') {
+        operator = e;
+    }
 
-function checkKey(e) {
-  if (
-    `.key[data-key="${e.keyCode}"]` == '192' ||
-    '111' ||
-    '106' ||
-    '109' ||
-    '107'
-  ) {
-    addOperator(e);
-  } else if (`.number[data-key="${e.keyCode}"]` == '46') {
-    clear();
-  } else if (`.number[data-key="${e.keyCode}"]` == '13') {
-    operate(e);
-  } else {
-    addNumber(e);
-  }
-}*/
+    previousNumber.textContent = `${result} ${operator}`;
+    currentNumber.textContent = result;
+    a = '';
+};
 
-function addNumber(e) {
-  // const keyNumber = document.querySelector(`.number[data-key="${e.keyCode}"]`);
-  currentNumber.textContent += e.target.textContent;
-  // currentNumber.textContent += keyNumber.value;
-  if (counter == 0) {
-    a += parseInt(e.target.textContent);
-  } else if (counter == 1) {
-    b += parseInt(e.target.textContent);
-  } else if (counter > 1 && counter % 2 == 0) {
-    c += parseInt(e.target.textContent);
-  } else {
-    b += parseInt(e.target.textContent);
-  }
-  console.log('a:', a);
-  console.log('b:', b);
-  console.log('c:', c);
-  console.log('operator:', operator);
-  console.log('counter:', counter);
-}
-
-const operatorsButton = document.querySelectorAll('.operator');
-operatorsButton.forEach((button) => {
-  button.addEventListener('click', addOperator);
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', handleClickOperatorButton);
 });
 
-function addOperator(e) {
-  previousNumber.textContent +=
-    currentNumber.textContent + ' ' + e.target.textContent + ' ';
-  newOperator = e.target.textContent;
-  counter++;
-  // operate(operator, a, b);
-  // a.textContent = currentNumber.textContent;
+const handleClickEqualButton = () => {
+    result = operate(operator, result, parseInt(a));
 
-  if (counter >= 1 && c == '' /*counter % 2 == 0*/) {
-    // a = parseInt(a) + parseInt(b);
-    operate(operator, a, b);
-    a = result;
-    b = '';
-  } else if (counter > 1 && b == '' /*counter % 2 !== 0*/) {
-    // a = parseInt(a) + parseInt(c);
-    operate(operator, a, parseInt(c));
-    a = result;
-    c = '';
-  }
-  operator = newOperator;
-  newOperator = '';
-  clearCurrent();
-}
-
-function clearCurrent() {
-  currentNumber.textContent = '';
-}
+    previousNumber.textContent = null;
+    currentNumber.textContent = result;
+    a = 0;
+};
 
 const equalButton = document.querySelector('.equal');
-equalButton.addEventListener('click', () => operate(operator, a, b));
+equalButton.addEventListener('click', handleClickEqualButton);
 
-function operate(operator, a, b) {
-  // previousNumber.textContent += currentNumber.textContent;
-  clearCurrent();
-  switch (operator) {
-    case '+':
-      result = add(parseInt(a), parseInt(b));
-      break;
-    case '-':
-      result = substract(parseInt(a), parseInt(b));
-      break;
-    case '*':
-      result = multiply(parseInt(a), parseInt(b));
-      break;
-    case '/':
-      result = divide(parseInt(a), parseInt(b));
-      break;
-  }
-  currentNumber.textContent = result;
-  console.log('result:', currentNumber.textContent);
-}
+const handleClickClearButton = () => {
+    a = '';
+    result = '';
+    operator = '';
 
-function add(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-// console.log(a);
-// console.log(b);
-// console.log(operator);
-// console.log(result);
-
-/*function equals() {
-  previousNumber.textContent += currentNumber.textContent + ' ' + '=';
-  const arr = previousNumber.textContent.split(' ');
-  operate(arr[1], arr[0], arr[2]);
-}*/
-
-/*function equals() {
-  const arr = previousNumber.textContent.split(' ');
-  for (let i = 0; i < arr.length; i++) {
-    if ((arr[1] = '+')) {
-      result = add(arr[0], arr[2]);
-    } else if ((arr[1] = '-')) {
-      result = substract(arr[0], arr[2]);
-    } else if ((arr[1] = '*')) {
-      result = multiply(arr[0], arr[2]);
-    } else if ((arr[1] = '/')) {
-      result = divide(arr[0], arr[2]);
-    }
-    currentNumber.textContent = result;
-  }
-  console.log(arr);
-}*/
+    previousNumber.textContent = null;
+    currentNumber.textContent = null;
+};
 
 const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', clear);
+clearButton.addEventListener('click', handleClickClearButton);
 
-function clear() {
-  counter = 0;
-  a = '';
-  b = '';
-  currentNumber.textContent = '';
-  previousNumber.textContent = '';
-}
+const handleKeyDown = (e) => {
+    console.log(e);
+    if (e.code.includes('Numpad') || e.code.includes('Digit')) {
+        if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+            handleClickOperatorButton(e.key);
+        } else if (e.key === 'Enter' || e.key === ' ') {
+            handleClickEqualButton();
+        } else {
+            handleClickNumberButton(e.key);
+        }
+    } else if (e.key === 'Backspace' || e.key === 'Escape' || e.key === 'Delete') {
+        handleClickClearButton();
+    }
+};
+
+window.addEventListener('keydown', handleKeyDown);
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+
+        case '-':
+            return subtract(a, b);
+
+        case '*':
+            return multiply(a, b);
+
+        case '/':
+            return divide(a, b);
+    }
+};
+
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
